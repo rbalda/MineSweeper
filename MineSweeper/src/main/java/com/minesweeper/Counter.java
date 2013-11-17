6b56ea7a9ba673fbf2ad4e8ba2e882489448be79
Rene Balda <rbalda@espol.edu.ec>
2013-11-17 16:09:35 -0500

    Creando clase count que llevara la informacion de cuantas banderas estand disponibles para colocar sobre minas

--- MineSweeper/src/main/java/com/minesweeper/Counter.java
+++ MineSweeper/src/main/java/com/minesweeper/Counter.java
@@ -5,7 +5,7 @@ import android.widget.TextView;
 /**
  * Created by ReneAlexander on 13/11/13.
  */
-public class Counter implements Observer {
+public class Counter {
     private int count;
     private TextView counterGUI;
 
@@ -18,13 +18,13 @@ public class Counter implements Observer {
         this.count = count;
     }
 
-    @Override
+  /*  @Override
     public void update() {
         if (count > 0) {
             count--;
             counterGUI.setText(Integer.toString(count));
         }
-    }
+    }*/
 
     public int getCount() {
         return count;
