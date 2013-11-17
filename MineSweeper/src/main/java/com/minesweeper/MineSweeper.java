70a00bedd79e89400b1055f8c74779f918a86934
Rene Balda <rbalda@espol.edu.ec>
2013-11-17 16:13:52 -0500

    Cambiando el tipo de boton que vamos a usar de ImageButto por ImageView para hacer pruebas de zoom

--- MineSweeper/src/main/java/com/minesweeper/MineSweeper.java
+++ MineSweeper/src/main/java/com/minesweeper/MineSweeper.java
@@ -1,10 +1,12 @@
 package com.minesweeper;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.LinearLayout;
 import android.widget.TableRow;
@@ -19,9 +21,12 @@ import java.util.HashMap;
 public class MineSweeper extends Activity {
     private Clock clock;
     private TableLayout gamePanel; // table layout to add mines to
-    private ImageButton btnSmile;
+    private Smile btnSmile;
     private boolean isPressed = false;
     private HashMap<Level, LevelData> levels;
+    private ImageView shield;
+
+    private AnimationDrawable animationDrawable;
 
 
     private BlockUI blocks[][]; // blocks for mine field
@@ -38,32 +43,38 @@ public class MineSweeper extends Activity {
         levels.put(Level.BEGINNER, new LevelData(9, 9, 10));
         levels.put(Level.INTERMEDIATE, new LevelData(16, 16, 40));
         levels.put(Level.EXPERT, new LevelData(16, 30, 99));
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.minesweeper_layout);
         TextView t = (TextView) findViewById(R.id.Clock);
+        shield = (ImageView) findViewById(R.id.flag);
+        shield.setBackgroundResource(R.drawable.shield);
+        animationDrawable = (AnimationDrawable) shield.getBackground();
         clock = new Clock(t);
 
         gamePanel = (TableLayout) findViewById(R.id.game_panel);
 
 
-        btnSmile = (ImageButton) findViewById(R.id.Smile);
+        btnSmile = new Smile((ImageButton) findViewById(R.id.Smile));
 
-        btnSmile.setOnClickListener(new View.OnClickListener() {
+        btnSmile.getButton().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 createGamePanel();
                 showGamePanel();
                 clock.start();
+                animationDrawable.start();
+
             }
         });
 
 
     }
+
 
     public void startGame() {
 
@@ -75,6 +86,7 @@ public class MineSweeper extends Activity {
         for (int row = 0; row < nOrInGamePanel + 2; row++) {
             for (int column = 0; column < nOcInGamePanel + 2; column++) {
                 blocks[row][column] = new BlockUI(this);
+                blocks[row][column].setSmile(btnSmile);
                 //blocks[row][column].setDefaults();
 
                 final int cRow = row;
@@ -102,7 +114,7 @@ public class MineSweeper extends Activity {
             tableRow.setLayoutParams(new LayoutParams((blockDimension + 2 * blockPadding) * nOcInGamePanel, blockDimension + 2 * blockPadding));
             tableRow.setGravity(android.view.Gravity.CENTER_HORIZONTAL);
             for (int column = 1; column < nOcInGamePanel + 1; column++) {
-                blocks[row][column].setLayoutParams(new LayoutParams(26, 26));
+                blocks[row][column].setLayoutParams(new LayoutParams(60, 60));
                 blocks[row][column].setPadding(blockPadding, blockPadding, blockPadding, blockPadding);
                 tableRow.addView(blocks[row][column]);
             }
