054f30a41ff034b0a03c2626cd4a9eb842f52b6c
Rene Balda <rbalda@espol.edu.ec>
2013-11-19 21:29:53 -0500

    modificando Shield para que se ajuste dentre de BlockUI

--- MineSweeper/src/main/java/com/minesweeper/Shield.java
+++ MineSweeper/src/main/java/com/minesweeper/Shield.java
@@ -21,6 +21,8 @@ public class Shield {
 
     public Shield(ImageView s) {
         this.s = s;
+        s.setScaleX((float) .5);
+        s.setScaleY((float) .5);
         s.setBackgroundResource(R.drawable.shield);
         s.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -30,15 +32,15 @@ public class Shield {
                     case MotionEvent.ACTION_DOWN:
                         oldX = (int) getS().getX();
                         oldY = (int) getS().getY();
-                        getS().setScaleX((float) 0.45);
-                        getS().setScaleY((float) 0.45);
+                        //getS().setScaleX((float) 0.45);
+                        //getS().setScaleY((float) 0.45);
                         View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
                         v.startDrag(null, shadowBuilder, v, 0);
                         break;
-                    case MotionEvent.ACTION_MOVE:
-                        getS().setX((int) event.getRawX() - getS().getWidth() / 2);
-                        getS().setY((int) event.getRawY() - getS().getHeight() / 2);
-                        break;
+                    //case MotionEvent.ACTION_MOVE:
+                    //   getS().setX((int) event.getRawX() - getS().getWidth() / 2);
+                    //  getS().setY((int) event.getRawY() - getS().getHeight() / 2);
+                    //  break;
                     case MotionEvent.ACTION_UP:
                         if (!(v.getParent() instanceof BlockUI)) {
                             getS().setX((float) oldX);
