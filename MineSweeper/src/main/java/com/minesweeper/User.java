7889085edeab7f837fb1085ef9d44727f4706402
Rene Alexander Balda Tarira <rbalda@espol.edu.ec>
2013-12-09 04:22:19 -0500

    Creando clase Usuario que guardara la informacion del usuario al guardar la partida

--- MineSweeper/src/main/java/com/minesweeper/User.java
+++ MineSweeper/src/main/java/com/minesweeper/User.java
@@ -7,4 +7,37 @@ public class User {
     private int id;
     private int time;
     private String initials;
+    private String level;
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public String getInitials() {
+        return initials;
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
 }
